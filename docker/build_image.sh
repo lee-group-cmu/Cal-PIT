6de5fc6a01272f4a19b223d52763d2f17ba69872
biprateep <biprateep@pitt.edu>
2023-07-28 10:33:13 -0700

    update docker build

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,4 +1,4 @@
 NVC_TAG=22.04
-NERSC_TAG="ngc-${NVC_TAG}-v0"
+MY_TAG="ngc-${NVC_TAG}-v0"
 
-docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t nersc/pytorch:$NERSC_TAG -f Dockerfile .
+docker build --platform linux/amd64 --build-arg nvc_tag=$NVC_TAG-py3 -t biprateep/pytorch:$MY_TAG -f Dockerfile .
