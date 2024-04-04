ce58846f4f307530efbb243ff84851dc24631b3b
biprateep <biprateep@gmail.com>
2024-04-04 19:59:35 -0400

    resolve merge conflicts

--- src/calpit/__init__.py
+++ src/calpit/__init__.py
@@ -1,1 +1,7 @@
+<<<<<<< before updating
 from .diagnostics_and_calibration import CalPitModel  # noqa: F401
+=======
+from .example_module import greetings, meaning
+
+__all__ = ["greetings", "meaning"]
+>>>>>>> after updating
