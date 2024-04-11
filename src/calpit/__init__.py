b0e2c91f23be4ec426c47e6eb8a38a125ec14292
biprateep <biprateep@gmail.com>
2024-04-11 11:57:29 -0400

    update template

--- src/calpit/__init__.py
+++ src/calpit/__init__.py
@@ -1,3 +1,9 @@
+<<<<<<< before updating
 from .diagnostics_and_calibration import CalPit
 
 __all__ = ["CalPit"]
+=======
+from .example_module import greetings, meaning
+
+__all__ = ["greetings", "meaning"]
+>>>>>>> after updating
